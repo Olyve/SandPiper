1cd5ae97b45cd4820cc623320673e9b00cd5d41d
pomfy113 <pomfy113@gmail.com>
2018-04-13 12:38:35 -0700

    Select all complete

--- sandpiper-frontend/src/components/Dashboard/Track/Track.js
+++ sandpiper-frontend/src/components/Dashboard/Track/Track.js
@@ -7,6 +7,7 @@ class TrackList extends Component {
 
       this.state = {
           site: this.props.site,
+          allTracks: [],
           selected: []
       }
   }
@@ -14,7 +15,7 @@ class TrackList extends Component {
   handleSubmit(event) {
       event.preventDefault();
       const filtered = this.state.selected.filter(content => content);
-      console.log(filtered);
+      console.log(filtered)
   }
 
   addToQueue(id, index){
@@ -28,20 +29,64 @@ class TrackList extends Component {
       }
 
       this.setState({
-          selected: listCopy
+          selected: listCopy,
+          checkAll: false
       })
+  }
+
+  checkAll(){
+      this.setState({ checkAll: true, selected: this.allTrackData })
+  }
+
+  cleanTrackData(track){
+      let albumImage, trackName, trackUrl, artistName, artistUrl, trackData, id;
+      switch(this.props.site){
+          case 'spotify':
+              trackData = track.track
+
+              id = trackData.external_ids.isrc;
+              albumImage = trackData.album.images[0].url || null;
+              trackName = trackData.name;
+              trackUrl = trackData.external_urls.spotify;
+              artistName = trackData.artists[0].name;
+              artistUrl = trackData.artists[0].external_urls.spotify;
+              break;
+          case 'itunes':
+              trackData = trackData.attributes;
+
+              id = trackData.id;
+              albumImage = trackData.artwork.url.replace(/(\{\w\})/g, '100');
+              trackName = trackData.name;
+              trackUrl = null;
+              artistName = trackData.artistName;
+              artistUrl = null;
+              break;
+          default:
+              return null;
+      }
+
+      return({albumImage, trackName, trackUrl, artistName, artistUrl, trackData, id})
   }
 
   render() {
     const tracks = this.props.tracks;
     const playlist = this.props.playlist
-    var trackList = [];
+
+    let trackList = [];
+    let allTrackData = [];
+
     if (tracks !== undefined) {
-      trackList = tracks.map((trackData, index) => {
-        return <Track site={this.props.site} key={index} track={trackData}
-                index={index} add={(id, index) => this.addToQueue(id, index)}/>
-      });
+          trackList = tracks.map((trackData, index) => {
+              const data = this.cleanTrackData(trackData);
+              allTrackData.push({id: data.id, name: data.trackName});
+
+              return <Track site={this.props.site} key={index} checked={this.state.selected[index]}
+                        index={index} add={(id, index) => this.addToQueue(id, index)} data={data}/>
+          });
+
+          this.allTrackData = allTrackData;
     }
+
 
     let imageURL, subtitle, name, url, embed;
     switch(this.props.site){
@@ -55,7 +100,7 @@ class TrackList extends Component {
                     <h3 className='playlist-tracks'>{`${playlist.tracks.total} tracks`}</h3>
                 </div>
             )
-            embed = <iframe title="spotify-playlist" className='tracklist-playlist-embed' width="400" height="280" frameBorder="0" 
+            embed = <iframe title="spotify-playlist" className='tracklist-playlist-embed' width="400" height="280" frameBorder="0"
                             src={`https://open.spotify.com/embed?uri=${playlist.uri}`} allowtransparency="true" allow="encrypted-media"/>
             break;
         case 'itunes':
@@ -94,6 +139,7 @@ class TrackList extends Component {
           </div>
           <form onSubmit={(ev) => this.handleSubmit(ev)}>
               <input type="submit" value="Submit"/>
+              <button onClick={() => this.checkAll()}>Check All</button>
               <div className='trackList'>{trackList}</div>
           </form>
       </div>
@@ -107,44 +153,16 @@ export class Track extends Component {
   }
 
   render() {
-    let albumImage, trackName, trackUrl, artistName, artistUrl, trackData, id;
-    switch(this.props.site){
-        case 'spotify':
-            trackData = this.props.track.track
-
-            id = trackData.external_ids.isrc;
-            albumImage = trackData.album.images[0].url || null;
-            trackName = trackData.name;
-            trackUrl = trackData.external_urls.spotify;
-            artistName = trackData.artists[0].name;
-            artistUrl = trackData.artists[0].external_urls.spotify;
-            break;
-        case 'itunes':
-            trackData = this.props.track.attributes;
-
-            id = trackData.id;
-            albumImage = trackData.artwork.url.replace(/(\{\w\})/g, '100');
-            trackName = trackData.name;
-            trackUrl = null;
-            artistName = trackData.artistName;
-            artistUrl = null;
-            break;
-        default:
-            return (<div>Track not available</div>);
-    }
-
-
-
     return (
       <div className='track'>
-        <input type='checkbox' name='track-select' value={id}
-                onChange={() => this.props.add({id: id, name: trackName}, this.props.index)}/>
+        <input type='checkbox' checked={this.props.checked ? true : false} name='track-select' value={this.props.data.id}
+                onChange={() => this.props.add({id: this.props.data.id, name: this.props.data.trackName}, this.props.index)}/>
         <div className='track-album'>
-          <img src={albumImage} height={100} width={100} alt={'Album artwork.'}/>
+          <img src={this.props.data.albumImage} height={100} width={100} alt={'Album artwork.'}/>
         </div>
         <div className='track-details'>
-          <a className='track-details-title' href={trackUrl} target='_blank'>{trackName}</a>
-          <a className='track-details-artist' href={artistUrl} target='_blank'>{artistName}</a>
+          <a className='track-details-title' href={this.props.data.trackUrl} target='_blank'>{this.props.data.trackName}</a>
+          <a className='track-details-artist' href={this.props.data.artistUrl} target='_blank'>{this.props.data.artistName}</a>
         </div>
       </div>
     );
